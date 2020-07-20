707a9edcd2abbc93febefa4cd8125eeb6541a4f4
heresson <heresson@gmail.com>
2020-07-20 18:32:20 -0300

    Estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
+
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
+
 // import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
